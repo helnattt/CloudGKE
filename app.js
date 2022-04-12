9b19e8c8b99bf3f0ae907c0c50563d3558686401
Helen Natalia <84366306+helnattt@users.noreply.github.com>
2022-04-12 15:46:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Nama: Helen Natalia <br> Nim: 51019010 <br> Jurusan: Sistem Informasi\n')
 })
 
 app.listen(PORT, HOST)
